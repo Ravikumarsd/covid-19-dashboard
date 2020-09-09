76de36311d595a345d99440ea85efab473ba1d12
Ravikumarsd <rvdvjn@gmail.com>
2020-09-10 04:56:36 +0530

    Updates

---
+++ precache-manifest.16d854483fe52e89a0ca4cc48fe5ace5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12de053b8d9bd9bf251470b8fffc315",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "12289793b520581abff0",
+    "url": "/covid-19-dashboard/static/css/main.5ad54249.chunk.css"
+  },
+  {
+    "revision": "5a1304a229aa4ee55402",
+    "url": "/covid-19-dashboard/static/js/2.fd4a103b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-dashboard/static/js/2.fd4a103b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12289793b520581abff0",
+    "url": "/covid-19-dashboard/static/js/main.78c9824b.chunk.js"
+  },
+  {
+    "revision": "0d5878ec4bbcb5c67f1a",
+    "url": "/covid-19-dashboard/static/js/runtime-main.8d77b089.js"
+  }
+]);
